de2347a44c44f5863ef7f1a90e0ef188f1333d61
AzhagammaiM <azhagammaimuthugopalakrishnan@gmail.com>
2025-08-16 14:43:17 +0530

    Time: 0 ms (100.00%) | Memory: 50.1 MB (91.65%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,17 +1,13 @@
 class Solution {
     public int singleNonDuplicate(int[] arr) {
-        // int left=0,right=arr.length-1;
-        // while(left<right){
-        //     int mid=left+(right-left)/2;
-        //     if(mid%2==1)mid-=1;
-        //     if(arr[mid]!=arr[mid+1])right=mid;
-        //     else left=mid+2;
-        // }
-        // return arr[left];
-         int result = 0;
-        for (int num : arr) {
-            result ^= num; // XOR all elements
+        int left=0,right=arr.length-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(mid%2==1)mid-=1;
+            if(arr[mid]!=arr[mid+1])right=mid;
+            else left=mid+2;
         }
-        return result; // Remaining number is the single element
+        return arr[left];
+        
     }
 }
